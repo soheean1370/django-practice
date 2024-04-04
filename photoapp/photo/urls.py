1fc6d338a9d88f937fa463901e7faa9f16aeb7f8
안소희 <127065983+soheean1370@users.noreply.github.com>
2024-04-05 01:29:54 +0900

    Update urls.py

--- photoapp/photo/urls.py
+++ photoapp/photo/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('photo/<int:pk>/', views.photo_detail, name = 'photo_detail'),
     path('photo/new/', views.photo_post, name=  'photo_post'),
     path('photo/<int:pk>/edit', views.photo_edit, name='photo_edit'),
-]
+] 
